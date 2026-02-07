82bbe45c18d9e8257b7464a648dd06b26dbba8c8
Frenchitas <134218245+Frenchitas@users.noreply.github.com>
2026-02-07 07:58:21 -0300

    vite

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
- base: '/portfolio/',
+ base: '/CSS-Responsive-Portfolio/',
  resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
